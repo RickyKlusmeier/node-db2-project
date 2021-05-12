60dab708cc3758c707ddf9e6c2943f769132c908
Ricky Klusmeier <rickyklusmeier@gmail.com>
2021-05-12 13:06:10 -0600

    MVP

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,38 @@
 // DO YOUR MAGIC
+const router = require('express').Router()
+const Cars = require('./cars-model')
+
+const {
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique
+  } = require('./cars-middleware')
+
+router.get('/', (req, res, next) => {
+    Cars.getAll()
+    .then(cars => {
+        res.status(200).json(cars)
+    })
+    .catch(next)
+})
+
+router.get('/:id', checkCarId, (req, res, next) => {
+    Cars.getById(req.params.id)
+    .then(car => {
+        res.status(200).json(car)
+    })
+    .catch(err => {
+        next(err)
+    })
+})
+
+router.post('/', checkCarPayload, checkVinNumberValid, checkVinNumberUnique, (req, res, next) => {
+    Cars.create(req.body)
+    .then(car => {
+        res.status(201).json(car)
+    })
+    .catch(next)
+})
+
+module.exports = router 
